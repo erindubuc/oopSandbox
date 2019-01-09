a76a1e0375c50bfb8b2d61e334d6b83fbae12cd7
erindubuc <erin.dubuc9@gmail.com>
2019-01-08 21:38:57 -0500

    Created new classes to demonstrate inheritance and hierarchy

--- Program.cs
+++ Program.cs
@@ -4,24 +4,60 @@ namespace Bulldozer
 {
     class MainClass
     {
+
         public static void Main(string[] args)
         {
-            var dozer = new BullDozer("Yellow", "Mini");
-
-            var dozer2 = new BullDozer("Red", "Large");
-           
-            Console.WriteLine("Bulldozer 1 is : " + dozer.color);
-            Console.WriteLine("Bulldozer 2 is : " + dozer2.color);
+            var man = new Man();
+            var kart = new Kart();
+            var alien = new Alien();
         }
     }
-    public class BullDozer
-    {
-        public BullDozer (string _color, string _size) {
-            this.color = _color;
-            this.size = _size;
-        }
-        public string color { get; set; }
-        public string size { get; set; }
 
+    public class GeneralObject
+    { 
+        public GeneralObject() {
+        }
+
+        public bool moves { get; set; }
+        public string color { get; set; }
+    }
+
+        public class Human : GeneralObject
+        { 
+            public Human () { 
+            }
+
+            public string name { get; set; }
+        }
+
+            public class Man : Human
+            {
+                public Man () {
+                }
+               
+            }
+
+            public class Woman : Human
+            { 
+                public Woman () { 
+                }
+            }
+
+    public class Kart : GeneralObject
+    {
+        public Kart () { 
+        }
+
+        
+        public string manufacturer { get; set; }
+    }
+
+    public class Alien : GeneralObject
+    {
+        public Alien()
+        {
+        }
+
+        public string planet { get; set; }
     }
 }
