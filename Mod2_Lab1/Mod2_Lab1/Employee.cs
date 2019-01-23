6f248de1f60162c5207cf7daffaf16d0ce9d8de9
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-01-22 22:19:27 -0500

    Created Employee Class for program for a company structure

---
+++ Mod2_Lab1/Mod2_Lab1/Employee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab1
+{
+    class Employee
+    {
+        // 3 private member variables: employeeName, employeeBaseSalary, and employeeId
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+
+        // Creates an integer variable called "employeeCount" and assigns value to 1
+        private static int employeeCount = 1;
+
+        // Public properties
+        public string Name
+        {
+            get
+            {
+                return employeeName;
+            }
+
+            set
+            {
+                employeeName = value;
+            }
+        }
+
+        // This method returns the employee's ID
+        public int getEmployeeID()
+        {
+            return this.ID;
+        }
+
+        // This method returns the employee's ID and Name
+        public String toString()
+        {
+            return this.ID + " " + this.Name;
+        }
+
+        // This method returns the employee's ID and Name and confirms that the employee is in the system
+        public virtual String employeeStatus()
+        {
+            return toString() + " is in the company's system.";
+        }
+
+        public double BaseSalary
+        {
+            get
+            {
+                return employeeBaseSalary;
+            }
+
+            set
+            {
+                employeeBaseSalary = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return employeeId;
+            }
+
+            set
+            {
+                employeeId = value;
+            }
+        }
+
+        // Constructor
+        public Employee(String name, double baseSalary)
+        {
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+            this.ID = employeeCount++;
+        }
+
+        // This method returns the employee's base salary
+        public double getBaseSalary()
+        {
+            return this.BaseSalary;
+        }
+
+        // This method returns the employee's name
+        public String getName()
+        {
+            return this.Name;
+        }
+
+    }
+}
