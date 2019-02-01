b015e763edc791bb526ee51394731047c76a6079
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-01 11:55:28 -0500

    Created new assembly that scrapes webpage content

---
+++ 2DArrays/2DArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace _2DArrays
+{
+    class Solution
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+                Console.WriteLine(arrTemp);
+            }
+            Console.ReadLine();
+        }
+    }
+}
