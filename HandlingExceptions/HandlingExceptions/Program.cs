448de327808948a31bb40d445c2a6c80ee425e2b
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-05 15:00:46 -0500

    Created exception messages specific to possible errors in app

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Example.txt");
+                Console.WriteLine(content);
+            }
+            // Check the most specific exceptions first
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is named correctly: Exampl.txt");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the directory is named correctly: C:\\Lesson22 exists");
+            }
+            // Check most generic exceptions last
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Code to finalize, set objects to null, close database connections, etc.
+                Console.WriteLine("Closing application...");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
